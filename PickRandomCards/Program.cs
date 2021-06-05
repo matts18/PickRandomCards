35d29a7bce42a797b0d9a6b1ab9111e10a2e2a0b
matts18 <snelgarm@gmail.com>
2021-06-06 07:29:47 +0800

    add CardPicker class

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -6,7 +6,24 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of cards to pick: ");
+
+            string line = Console.ReadLine();
+
+            if(int.TryParse(line,out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+
+                foreach(var card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry");
+            }
+
         }
     }
 }
